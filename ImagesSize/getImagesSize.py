fab0ba337506b95801ad53efddf9406c66d35bc8
ezus-melchior <117109343+ezus-melchior@users.noreply.github.com>
2023-04-19 17:57:11 +0200

    Update getImagesSize.py

--- ImagesSize/getImagesSize.py
+++ ImagesSize/getImagesSize.py
@@ -9,20 +9,32 @@ from io import BytesIO
 env ="esus_dev"
 #### END OFVARIABLE #####
 def get_image_size(url):
-    data = requests.get(url).content
-    im = Image.open(BytesIO(data))
-    return str(len(im.fp.read()))
+    try:
+        data = requests.get(url).content
+        im = Image.open(BytesIO(data))
+        return str(len(im.fp.read()))
+    except:
+        return -1
 
 conn, tunnel = conn_init(env)
 cur = conn.cursor()
 
 print("Start")
 
-cur.execute("SELECT id, path_full FROM media")
+cur.execute("SELECT DISTINCT path_full FROM media_activities WHERE deleted_at is NULL AND size is NULL")
 medias = cur.fetchall()
+size_values = []
 for media in medias:
-    print(get_image_size(media["path_full"]))
-    print(media["path_full"])
+    size_values.append([get_image_size(media["path_full"]), media["path_full"]])
+sql_query = "UPDATE media_activities SET size = %s WHERE path_full = %s"
+cur.executemany(sql_query, size_values)
+cur.execute("SELECT DISTINCT path_full FROM media WHERE deleted_at is NULL AND size is NULL")
+medias = cur.fetchall()
+size_values = []
+for media in medias:
+    size_values.append([get_image_size(media["path_full"]), media["path_full"]])
+sql_query = "UPDATE media SET size = %s WHERE path_full = %s"
+cur.executemany(sql_query, size_values)
 
 conn.commit()
 
